77e8113e9be029bba46aaf5532ab8c7ce35da4f5
Bret Lindquist <30852056+bretlindquist@users.noreply.github.com>
2025-02-13 20:07:30 +0900

    oops, deleted extra code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,43 +143,6 @@ const audioFiles: AudioFile[] = [
 ];
 
 
-interface AudioFile {
-  src: string;
-  title: string;
-}
-
-// Use your existing MP3 links and titles
-const audioFiles: AudioFile[] = [
-  {
-    src: "https://ucarecdn.com/46c9f4ee-f6f9-467a-a2f3-71d5f4503376/BretLindquist2025Samples.mp3",
-    title: "Brets Reel",
-  },
-  {
-    src: "https://ucarecdn.com/93e6ae68-18a5-4253-8e5d-6174f4c608f9/2025BretCharDemo.mp3",
-    title: "Characters",
-  },
-  {
-    src: "https://ucarecdn.com/237b8f2e-4b83-457f-8740-0e85f069a004/VariousCharacters.mp3",
-    title: "Characters More",
-  },
-  {
-    src: "https://ucarecdn.com/1e10d202-e465-4f1a-a9477-8630078312ef/calltoduty4.mp3",
-    title: "TV Ad",
-  },
-  {
-    src: "https://ucarecdn.com/a5879b78-89a7-483d-b668-aa1c423fa1a8/firecountry.mp3",
-    title: "TV Prime Time",
-  },
-  {
-    src: "https://ucarecdn.com/c5ad268d-24f5-47f6-a52c-5f2bd4f9d9b7/Project1.mp3",
-    title: "Video Game",
-  },
-  {
-    src: "https://ucarecdn.com/5adfdf11-a726-4abb-820d-3969b4b3d07b/rainforests_of_borneo5.mp3",
-    title: "Narration",
-  },
-];
-
 function VoiceActingSection() {
   // 1) Track which audio file is selected
   const [currentIndex, setCurrentIndex] = useState<number | null>(null);
