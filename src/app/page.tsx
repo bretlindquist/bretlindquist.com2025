e205b2a914da4e5e9bfb36cee3ea4ee6e5cd34e5
Bret Lindquist <30852056+bretlindquist@users.noreply.github.com>
2025-02-13 18:10:28 +0900

    removed top padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="pt-[60px]">
+      <main>
         <HeroSection />
         <ActingSection />
         <VoiceActingSection />
